93b4a4e69ee6674406cfc003e462e57f3637181b
Ken Warren <134076996+ken-warren@users.noreply.github.com>
2024-10-28 10:55:09 +0300

    Update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const bar = document.getElementById('bar')
-cons nav = document.getElementById('navbar')
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
 
 if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    })
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
 }
